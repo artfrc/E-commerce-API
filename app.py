e082587636937c73e3b131664a5ce8d4f4b86891
Arthur <crfernandesarthur@gmail.com>
2024-09-13 11:18:36 -0300

    Aula 3: implementação de: coluna usuário, função de login e logout, rotas de consultar detalhes de um produto, atualizar produto, ver todos os produtos e também funções de autenticação do usuário.

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy  
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my_key_123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
-
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+CORS(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=True, unique=True)
+    password = db.Column(db.String(80), nullable=True)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,8 +24,35 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    # username = data.get("username")
+    # password = data.get("password")
+
+    user = User.query.filter_by(username =data.get("username")).first()
+    
+    if user and data.get("password") == user.password:
+        login_user(user)
+        return  jsonify({'message': "Login feito com sucesso"})
+        
+    return  jsonify({'message': "Informações inválidas"}), 401
+
+@app.route('/logout', methods= ["POST"])
+@login_required
+def logout():
+    logout_user()
+    return  jsonify({'message': "Logout feito com sucesso"})
+
 
 @app.route('/api/products/add', methods=["POST"])
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -27,6 +65,7 @@ def add_product():
 
 
 @app.route('/api/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -36,6 +75,55 @@ def delete_product(product_id):
     
     return  jsonify({'message': "Produto não encontrado"}), 404
 
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return  jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    
+    return  jsonify({'message': "Produto não encontrado"}), 404
+
+@app.route('/api/update/<int:product_id>', methods=["PUT"])
+@login_required
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return  jsonify({'message': "Produto não encontrado"}), 404
+
+    data = request.json
+    if 'name' in data:
+        product.name = data["name"]
+
+    if 'price' in data:
+        product.price = data["price"]
+
+    if 'description' in data:
+        product.description = data["description"]
+
+    db.session.commit()
+    return  jsonify({'message': "Produto atualizado com sucesso!"})
+    
+@app.route('/api/products', methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    if len(products) == 0:
+        return  jsonify({'message': "Lista de produtos vazia"})
+    
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+        product_list.append(product_data)
+
+    return jsonify(product_list)
 
 @app.route('/')
 def hello_world():
