5613d6b6c0b7cac5e70a471fc5d3cc320caad643
Arthur <crfernandesarthur@gmail.com>
2024-09-12 23:28:28 -0300

    Aula 2: criação bd, criação de rotas add e delete produto.

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy  
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return  jsonify({'message': "Produto cadastrado com sucesso!"})
+    
+    return  jsonify({'message': "Dados do produto inválidos"}), 400
+
+
+@app.route('/api/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return  jsonify({'message': "Produto deletado com sucesso!"})
+    
+    return  jsonify({'message': "Produto não encontrado"}), 404
+
 
 @app.route('/')
 def hello_world():
